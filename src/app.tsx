d72760676d9c643d19b6603ed15c6c9b82682a3f
pedro henrique <pedro.henrike2812@gmail.com>
2024-03-04 10:06:21 -0300

    initial commit

---
+++ src/app.tsx
@@ -0,0 +1,12 @@
+import { Circle } from "./components/Circle";
+
+export function App() {
+  return (
+      <section className="w-full mt-6 flex items-center justify-center gap-6">
+        <Circle />
+        <Circle />
+        <Circle />
+      </section>
+  )
+}
+
---
+++ src/components/Circle.tsx
@@ -0,0 +1,5 @@
+export function Circle  ()  {
+  return (
+    <div className="w-28 h-28 rounded-full bg-red-600"></div>
+  )
+};
